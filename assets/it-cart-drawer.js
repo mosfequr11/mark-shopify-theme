cd21733f5b2438571cd7991a13fa230cd63bbde9
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-01-19 23:46:27 +0600

    add: added js file to open and close the cart drawer

---
+++ assets/it-cart-drawer.js
@@ -0,0 +1,8 @@
+// open Cart Drawer
+function openCartDrawer() {
+  document.querySelector(".it-cart-drawer").classList.add("drawer_active");
+}
+// Close Cart Drawer
+function closeCartDrawer() {
+  document.querySelector(".it-cart-drawer").classList.remove("drawer_active");
+}
