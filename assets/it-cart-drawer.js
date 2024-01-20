bfd6668380380313ef5c08cb028827a11b42c951
Md Mosfequr <97782191+mosfequr11@users.noreply.github.com>
2024-01-20 23:30:04 +0600

    add: Update cart count

--- assets/it-cart-drawer.js
+++ assets/it-cart-drawer.js
@@ -6,3 +6,23 @@ function openCartDrawer() {
 function closeCartDrawer() {
   document.querySelector(".it-cart-drawer").classList.remove("drawer_active");
 }
+
+// Update cart count
+function updateCartItemCounts(count) {
+  document.querySelectorAll(".cart-count").forEach((el) => {
+    el.textContent = count;
+  });
+}
+
+async function updateCartDrawer() {
+  const res = await fetch("/?section_id=cart-drawer");
+  const text = await res.text();
+  const html = document.createElement("div");
+  html.innerHTML = text;
+
+  const newBox = html.querySelector(".cart-drawer").innerHTML;
+
+  document.querySelector(".cart-drawer").innerHTML = newBox;
+
+  addCartDrawerListeners();
+}
