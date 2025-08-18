48b825a5910caf205560b9af1509876a423758e2
Md Mosfequr <mosfequr11@users.noreply.github.com>
2025-08-18 22:36:06 +0600

    add: product slider section

---
+++ assets/product-slider.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  breakpoints: {
+    480: {
+      // For devices >= 480px
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+    640: {
+      // For devices >= 640px
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      // For tablets and above
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
+    1024: {
+      // For desktops
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
